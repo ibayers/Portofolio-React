cb35ace0acaf3d8ab1eb5559a5a5b77a206601cb
ibayers <bryann.carls@gmail.com>
2026-05-14 20:34:50 +0700

    fix photo certif

--- src/components/certifSection.jsx
+++ src/components/certifSection.jsx
@@ -1,18 +1,29 @@
 import { CircularGallery } from "@/components/ui/circular-gallery-2";
+import cert1 from "@/assets/1.png";
+import cert2 from "@/assets/2.png";
+import cert3 from "@/assets/3.png";
+import cert4 from "@/assets/4.jpg";
+import cert5 from "@/assets/5.jpg";
+import cert6 from "@/assets/6.jpg";
+import cert7 from "@/assets/7.png";
+import cert8 from "@/assets/8.png";
+import cert9 from "@/assets/9.png";
+import cert10 from "@/assets/10.jpg";
+import cert11 from "@/assets/11.jpg";
 
 /** @type {Array<{image: string, text: string}>} */
 const galleryItems = [
-  { image: "/src/assets/1.png", text: "Bridge" },
-  { image: "/src/assets/2.png", text: "Bridge" },
-  { image: "/src/assets/3.png", text: "Bridge" },
-  { image: "/src/assets/4.jpg", text: "Bridge" },
-  { image: "/src/assets/5.jpg", text: "Bridge" },
-  { image: "/src/assets/6.jpg", text: "Bridge" },
-  { image: "/src/assets/7.png", text: "Bridge" },
-  { image: "/src/assets/8.png", text: "Bridge" },
-  { image: "/src/assets/9.png", text: "Bridge" },
-  { image: "/src/assets/10.jpg", text: "Bridge" },
-  { image: "/src/assets/11.jpg", text: "Bridge" },
+  { image: cert1, text: "Bridge" },
+  { image: cert2, text: "Bridge" },
+  { image: cert3, text: "Bridge" },
+  { image: cert4, text: "Bridge" },
+  { image: cert5, text: "Bridge" },
+  { image: cert6, text: "Bridge" },
+  { image: cert7, text: "Bridge" },
+  { image: cert8, text: "Bridge" },
+  { image: cert9, text: "Bridge" },
+  { image: cert10, text: "Bridge" },
+  { image: cert11, text: "Bridge" },
 ];
 
 export default function CircularGalleryDemo() {
